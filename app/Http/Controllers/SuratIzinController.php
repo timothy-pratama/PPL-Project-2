2c7a95b64e61b537f8ac6845605d9340f3e04e76
ichakid <choirunnisa.fatima@gmail.com>
2015-04-28 01:06:04 +0700

    Surat Izin with dummy data

--- app/Http/Controllers/SuratIzinController.php
+++ app/Http/Controllers/SuratIzinController.php
@@ -14,7 +14,25 @@ class SuratIzinController extends Controller
     public function index()
     {
     	$pdf = App::make('dompdf');
-        $pdf->loadView('SuratIzin')->setPaper('a4')->setOrientation('portrait')->setWarnings(false)->save('myfile.pdf');
+    	$parameter = array();
+    	$parameter['nomorsurat'] = "510/2 - 0077 -DISKUKM & PERINDAG/2008";
+    	$parameter['namaperusahaan'] = "PT. DUTA FUTURE INTERNATIONAL";
+    	$parameter['merek'] = "-";
+    	$parameter['alamatperusahaan'] = "Jl. Kuningan 13 no. 11 A Kel. Antapani Tengah Kecamatan Antapani Kota Bandung";
+    	$parameter['namapemilik'] = "Febrian Agung Budi Prastyo";
+    	$parameter['alamatpemilik'] = "Jl. Kuningan 15 no. 19 Kel. Antapani Tengah Kecamatan Antapani Kota Bandung";
+    	$parameter['npwp'] = "02.789.009.4 - 429.000";
+    	$parameter['nilaimodal'] = "Rp285.000.000,00";
+    	$parameter['kegiatanusaha'] = "Perdagangan Barang dan Jasa";
+    	$parameter['kelembagaan'] = "Pemasok";
+    	$parameter['bidangusaha'] = "-";
+    	$parameter['jenisbarang'] = "Alat, suku cadang, alat tulis, katering, komputer";
+    	$parameter['tanggal'] = "17 Maret 2008";
+    	$parameter['kecamatan'] = "Antapani";
+    	$parameter['namakepala'] = "Drs. H. Ernawan Natasaputra";
+    	$parameter['jabatankepala'] = "Pembina Utama Muda";
+    	$parameter['nipkepala'] = "190 000 393";
+        $pdf->loadView('SuratIzin', $parameter)->setPaper('folio')->setOrientation('portrait')->setWarnings(false)->save('myfile.pdf');
         return $pdf->stream();
     }
 }
--- resources/views/SuratIzin.php
+++ resources/views/SuratIzin.php
@@ -19,78 +19,78 @@
 		</table>
     	
     	<hr style="border: 3px outset #595955;">
-    	<br><br>
+    	<br>
     	<font size="13"><u><b>SURAT IZIN USAHA PERDAGANGAN (SIUP)</b></u></font><br>
-    	<font size="13"><b>NOMOR: </b></font><br><br><br>    	
+    	<font size="12"><b>NOMOR: <?php echo $nomorsurat; ?></b></font><br><br><br>    	
     </div>
     <div>
     	<table>
 		  	<tr>
-			    <td style="width:3px"><font size="12">1.</font></td>
-			    <td style="width:300px"><font size="12">Nama Perusahaan</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="width:3px"  style="vertical-align: top"><font size="12">1.</font></td>
+			    <td style="width:300px" style="vertical-align: top"><font size="12">Nama Perusahaan</font></td>
+			    <td style="width:3px" style="vertical-align: top">:</td>		
+			    <td><?php echo $namaperusahaan; ?></td>
 		  	</tr>
 		  	<tr>
-			    <td><font size="12">2.</font></td>
-			    <td><font size="12">Merek (milik sendiri/lisensi)</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><font size="12">2.</font></td>
+			    <td style="vertical-align: top"><font size="12">Merek (milik sendiri/lisensi)</font></td>
+			    <td style="vertical-align: top">:</td>		
+			    <td><?php echo $merek; ?></td>
 		  	</tr>
 		  	<tr>
-			    <td><font size="12">3.</font></td>
-			    <td><font size="12">Alamat Kantor Perusahaan</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><font size="12">3.</font></td>
+			    <td style="vertical-align: top"><font size="12">Alamat Kantor Perusahaan</font></td>
+			    <td style="vertical-align: top">:</td>		
+			    <td><?php echo $alamatperusahaan?></td>
 		  	</tr>
 		  	<tr>
-			    <td><font size="12">4.</font></td>
-			    <td><font size="12">Nama Pemilik/Penanggung Jawab</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><font size="12">4.</font></td>
+			    <td style="vertical-align: top"><font size="12">Nama Pemilik/Penanggung Jawab</font></td>
+			    <td style="vertical-align: top">:</td>		
+			    <td><?php echo $namapemilik; ?></td>
 		  	</tr>
 		  	<tr>
-			    <td><font size="12">5.</font></td>
-			    <td><font size="12">Alamat Pemilik/Penanggung Jawab</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><font size="12">5.</font></td>
+			    <td style="vertical-align: top"><font size="12">Alamat Pemilik/Penanggung Jawab</font></td>
+			    <td style="vertical-align: top">:</td>		
+			    <td><?php echo $alamatpemilik; ?></td>
 		  	</tr>
 		  	<tr>
 			    <td><font size="12">6.</font></td>
 			    <td><font size="12">Nomor Pokok Wajib Pajak</font></td>
 			    <td>:</td>		
-			    <td></td>
+			    <td><?php echo $npwp; ?></td>
 		  	</tr>
 		  	<tr>
 			    <td style="vertical-align: top"><font size="12">7.</font></td>
 			    <td><font size="12">Nilai Modal dan Kekayaan Bersih Perusahaan 
 			    	seluruhnya tidak termasuk Tanah dan Bangunan Tempat Berusaha</font></td>
 			    <td style="vertical-align: top">:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><?php echo $nilaimodal; ?></td>
 		  	</tr>
 		  	<tr>
-			    <td><font size="12">8.</font></td>
-			    <td><font size="12">Kegiatan Usaha</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><font size="12">8.</font></td>
+			    <td style="vertical-align: top"><font size="12">Kegiatan Usaha</font></td>
+			    <td style="vertical-align: top">:</td>		
+			    <td><?php echo $kegiatanusaha; ?></td>
 		  	</tr>
 		  	<tr>
-			    <td><font size="12">9.</font></td>
-			    <td><font size="12">Kelembagaan</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><font size="12">9.</font></td>
+			    <td style="vertical-align: top"><font size="12">Kelembagaan</font></td>
+			    <td style="vertical-align: top">:</td>		
+			    <td><?php echo $kelembagaan; ?></td>
 		  	</tr>
 		  	<tr>
-			    <td><font size="12">10.</font></td>
-			    <td><font size="12">Bidang Usaha</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><font size="12">10.</font></td>
+			    <td style="vertical-align: top"><font size="12">Bidang Usaha</font></td>
+			    <td style="vertical-align: top">:</td>		
+			    <td><?php echo $bidangusaha; ?></td>
 		  	</tr>
 		  	<tr>
-			    <td><font size="12">11.</font></td>
-			    <td><font size="12">Jenis Barang / Jasa Dagang Utama</font></td>
-			    <td>:</td>		
-			    <td></td>
+			    <td style="vertical-align: top"><font size="12">11.</font></td>
+			    <td style="vertical-align: top"><font size="12">Jenis Barang / Jasa Dagang Utama</font></td>
+			    <td style="vertical-align: top">:</td>		
+			    <td><?php echo $jenisbarang; ?></td>
 		  	</tr>
 		</table>
 		<br>
@@ -132,9 +132,10 @@
 			    <td>Tidak untuk melakukan kegiatan usaha selain yang tercantum dalam SIUP ini.</td>
 		  	</tr>
 		</table>
-		<table style="vertical-align: text-bottom">
+		<br>
+		<table style="vertical-align: text-bottom" width="100%">
 		  	<tr>
-			    <td style="vertical-align: bottom">
+			    <td style="vertical-align: bottom" width="60%">
 		    		<font size="10pt">Tembusan disampaikan kepada Yth. :</font><br>
 		    		<table>
 		    			<tr>
@@ -150,11 +151,11 @@
 		    			</tr>
 		    			<tr>
 		    				<td><font size="10pt">3.</font></td>
-		    				<td><font size="10pt">Kecamatan ..................</font></td>
+		    				<td><font size="10pt">Kecamatan <?php echo $kecamatan; ?></font></td>
 		    			</tr>
 		    		</table>
 		    	</td>	
-			    <td>
+			    <td width="40%">
 			    	<table>
 			    		<tr>
 			    			<td><font size="12">Dikeluarkan di</font></td>
@@ -164,16 +165,16 @@
 			    		<tr>
 			    			<td><font size="12">Pada tanggal</font></td>
 			    			<td><font size="12">:</font></td>
-			    			<td><font size="12"></font></td>
+			    			<td><font size="12"><?php echo $tanggal; ?></font></td>
 			    		</tr>
 			    	<table>
 		    		<div align="center">
 		    			<font size="12">DINAS KOPERASI UKM DAN <br>PERINDUSTRIAN PERDAGANGAN</font><br>
 		    			<font size="12">KOTA BANDUNG</font><br><br>	    			
 		    			<font size="12">Kepala,</font><br><br><br>	    			
-		    			<font size="12"><u></u></font><br>
-		    			<font size="12"></font><br>
-		    			<font size="12">NIP. </font><br>
+		    			<font size="12"><u><?php echo $namakepala; ?></u></font><br>
+		    			<font size="12"><?php echo $jabatankepala; ?></font><br>
+		    			<font size="12">NIP. <?php echo $nipkepala; ?></font><br>
 		    		</div>
 			    </td>
 		  	</tr>
