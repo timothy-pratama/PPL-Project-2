c1245f55611eebf84779aae4db8921b99d874e1c
Timothy Pratama <timothy.pratama@gmail.com>
2015-05-17 16:46:37 +0700

    Ganti email ke timothy.pratama@gmail.com (buat test).

--- app/Library/SendMail.php
+++ app/Library/SendMail.php
@@ -7,7 +7,8 @@ class SendMail
 	public static function sendMail(){
 		Mail::send('linkizin', [], function($message)
 		{
-			$message->to('riadysastrak@gmail.com', 'riady')->subject( "Persetujuan izin" );
+			//$message->to('riadysastrak@gmail.com', 'riady')->subject( "Persetujuan izin" );
+                $message->to('timothy.pratama@gmail.com', 'Timothy')->subject( "Persetujuan Izin" );
 		});
 	}
 	
@@ -15,7 +16,8 @@ class SendMail
 		$a = 'Berikut tautan menuju dokumen persetujuan http://localhost:8000/suratizin/view/'.$id;
 		Mail::raw($a, function($message)
 		{
-			$message->to('riadysastrak@gmail.com', 'riady')->subject( "Dokumen persetujuan" );
+			//$message->to('riadysastrak@gmail.com', 'riady')->subject( "Dokumen persetujuan" );
+            $message->to('timothy.pratama@gmail.com', 'Timothy')->subject( "Dokumen Persetujuan" );
 		});
 	}
 
